c95077fc237e9cacddfe65e32d2fa5535ac03f1f
bmappa1342 <bmappaisidk@groupeisi.com>
2022-10-09 11:28:09 +0200

    lancement du server au port 80

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+require('dotenv').config({path:'./config/.env'});
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+})
+
+ 

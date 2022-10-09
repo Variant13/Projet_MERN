800d1aee2342afe1b9885507eb43df7225877270
bmappa1342 <bmappaisidk@groupeisi.com>
2022-10-09 14:50:00 +0200

    installation of bcrypt for MDP

--- models/user.model.js
+++ models/user.model.js
@@ -1,48 +1,58 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-    
-  pseudo: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 30,
-    trim: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    pseudo: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 30,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minlength: 6,
+    },
+    picture: {
+      type: String,
+      default: "./uploads/profil/random-user.png",
+    },
+    bio: {
+      type: String,
+      max: 1024,
+    },
+    followers: {
+      type: [String],
+    },
+    following: {
+      type: [String],
+    },
+    likes: {
+      type: [String],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    validator: [isEmail],
-    lowercase: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-    max: 1024,
-    minlength: 6
-  },
-  bio : {
-    type: String,
-    max: 1024
-  },
-  followers: {
-    type: [String]
-  },
-  following: {
-    type: [String]
-  },
-  likes: {
-    type: [String]
-  },
- },
   {
     timestamps: true,
-      
   }
-)
+);
 
-const UserModel = mongoose.model('user',userSchema);
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;

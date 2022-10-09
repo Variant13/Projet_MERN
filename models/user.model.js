d37d787cab78b3181c059d963890895a8d449216
bmappa1342 <bmappaisidk@groupeisi.com>
2022-10-09 14:22:49 +0200

    installation of body-parser and inter-communication with cluster mongodb

--- models/user.model.js
+++ models/user.model.js
@@ -2,26 +2,47 @@ const mongoose = require("mongoose");
 const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
+    
   pseudo: {
     type: String,
     required: true,
     minLength: 3,
     maxLength: 30,
     trim: true,
-    unique: true,
+    unique: true
   },
   email: {
     type: String,
     required: true,
     validator: [isEmail],
     lowercase: true,
-    trim: true,
+    trim: true
   },
   password: {
     type: String,
     required: true,
     max: 1024,
-    trim: true,
-    minlength: 6,
+    minlength: 6
   },
-});
+  bio : {
+    type: String,
+    max: 1024
+  },
+  followers: {
+    type: [String]
+  },
+  following: {
+    type: [String]
+  },
+  likes: {
+    type: [String]
+  },
+ },
+  {
+    timestamps: true,
+      
+  }
+)
+
+const UserModel = mongoose.model('user',userSchema);
+module.exports = UserModel;
